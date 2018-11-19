69f3d8e76713a9cc9dea2c1a27207764418430c0
Daniel Mera de Agustin <daniel.meradeagustin@gmail.com>
2018-11-19 12:38:04 +0100

    ex1 without styling

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# This is my questions controller
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:question]
+    @answer = coach
+  end
+
+  def coach
+    if @question[@question.length - 1] == '?'
+      'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      'Great'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
